c417858a0747a88fab1b05cbc793ede55cceaafd
SamuelGoulart <62961331+SamuelGoulart@users.noreply.github.com>
2021-04-23 11:40:49 -0300

    Criação da activity imc

--- IMCAPP/app/src/main/java/com/example/imc_app/MainActivity.kt
+++ IMCAPP/app/src/main/java/com/example/imc_app/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.imc_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // ** Clique no card IMC
+        val cardImc = findViewById<CardView>(R.id.card_imc)
+
+        cardImc.setOnClickListener {
+            val abrirImcActivity = Intent(this, imcActivity::class.java)
+            startActivity(abrirImcActivity)
+
+        }
     }
+
 }
